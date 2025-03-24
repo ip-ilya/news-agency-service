87ca4e7f5c0700587c73d3ae80ec0c4105c331ed
Illia Khomutov <ilyakhomutov613@gmail.com>
2025-03-24 21:26:41 +0200

    add models for topic, redactor and newspaper

--- news_agency/models.py
+++ news_agency/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=63)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.username
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=63)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        related_name="newspapers"
+    )
+    publishers = models.ManyToManyField(
+        Redactor,
+        related_name="newspapers"
+    )
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ["-published_date"]
