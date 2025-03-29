226ff1be0144de10df186b249f4bdb8ea50dbc19
Illia Khomutov <ilyakhomutov613@gmail.com>
2025-03-29 12:35:55 +0200

    add urls, templates for login and logout

--- news_agency_service/urls.py
+++ news_agency_service/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("news_agency.urls", namespace="news_agency"))
+    path("", include("news_agency.urls", namespace="news_agency")),
+    path("accounts/", include("django.contrib.auth.urls"))
 ]
