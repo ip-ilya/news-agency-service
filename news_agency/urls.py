74650b2306614c15bd83d327ef6e3ade73909625
Illia Khomutov <ilyakhomutov613@gmail.com>
2025-04-04 14:37:39 +0300

    fix flake8 issues

--- news_agency/urls.py
+++ news_agency/urls.py
@@ -26,13 +26,27 @@ urlpatterns = [
     path("topics/", TopicListView.as_view(), name="topic-list"),
     path("topics/<int:pk>/", TopicDetailView.as_view(), name="topic-detail"),
     path("topics/create/", TopicCreateView.as_view(), name="topic-create"),
-    path("topics/<int:pk>/update/", TopicUpdateView.as_view(), name="topic-update"),
-    path("topics/<int:pk>/delete/", TopicDeleteView.as_view(), name="topic-delete"),
+    path(
+        "topics/<int:pk>/update/",
+        TopicUpdateView.as_view(),
+        name="topic-update"
+    ),
+    path(
+        "topics/<int:pk>/delete/",
+        TopicDeleteView.as_view(),
+        name="topic-delete"
+    ),
     path("newspapers/", NewspaperListView.as_view(), name="newspaper-list"),
     path(
-        "newspapers/<int:pk>/", NewspaperDetailView.as_view(), name="newspaper-detail"
+        "newspapers/<int:pk>/",
+        NewspaperDetailView.as_view(),
+        name="newspaper-detail"
     ),
-    path("newspapers/create/", NewspaperCreateView.as_view(), name="newspaper-create"),
+    path(
+        "newspapers/create/",
+        NewspaperCreateView.as_view(),
+        name="newspaper-create"
+    ),
     path(
         "newspapers/<int:pk>/update/",
         NewspaperUpdateView.as_view(),
@@ -44,8 +58,16 @@ urlpatterns = [
         name="newspaper-delete",
     ),
     path("redactors/", RedactorListView.as_view(), name="redactor-list"),
-    path("redactors/<int:pk>/", RedactorDetailView.as_view(), name="redactor-detail"),
-    path("redactors/create/", RedactorCreateView.as_view(), name="redactor-create"),
+    path(
+        "redactors/<int:pk>/",
+        RedactorDetailView.as_view(),
+        name="redactor-detail"
+    ),
+    path(
+        "redactors/create/",
+        RedactorCreateView.as_view(),
+        name="redactor-create"
+    ),
     path(
         "redactors/<int:pk>/update/",
         RedactorUpdateView.as_view(),
