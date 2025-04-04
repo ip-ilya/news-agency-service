40fcfbadcf7bb04a9da4610b6e80e9c00e673181
Illia Khomutov <ilyakhomutov613@gmail.com>
2025-04-04 19:36:04 +0300

    move SECRET_KEY to .env

--- news_agency_service/settings.py
+++ news_agency_service/settings.py
@@ -1,14 +1,15 @@
+import os
 from pathlib import Path
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-f3!!7$u9$ya!i^pz%%9i9@kjzfax4)ii6(=@!c53f$se+bpcxe"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
