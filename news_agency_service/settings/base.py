16dcf00db0921479267355c13eadd3d7f8888b08
Illia Khomutov <ilyakhomutov613@gmail.com>
2025-04-07 21:28:23 +0300

    update settings

--- news_agency_service/settings/base.py
+++ news_agency_service/settings/base.py
@@ -30,7 +30,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
